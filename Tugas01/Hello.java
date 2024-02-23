50415de781523aa31caff04502845a8069cdfdda
ZahraKintani <zahrakintani@gmail.com>
2024-02-23 08:39:54 +0700

    Update commit

--- Tugas01/Hello.java
+++ Tugas01/Hello.java
@@ -1,6 +1,7 @@
 package Tugas01;
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("halo semuanya");
+        System.out.println("halo");
+        System.out.println("i mean cubayiat");
     }
 }
