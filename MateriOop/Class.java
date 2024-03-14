e94144a920a324e3f08daa7d81b04596457c1667
ZahraKintani <zahrakintani@gmail.com>
2024-03-14 09:22:29 +0700

    Constructor

--- MateriOop/Class.java
+++ MateriOop/Class.java
@@ -3,16 +3,32 @@ package MateriOop;
 class Class {
     public static void main(String[] args) {
         Mobil mobil1 = new Mobil();
-
         mobil1.merk = "Range Rover";
         mobil1.platNomor = "N 7412 AH";
         mobil1.tahun = 2020;
         mobil1.transmisi = "Automatic";
+        System.out.println("Merk mobil anda: " + mobil1.merk + "\nPlat nomor: " + mobil1.platNomor + "\nKeluaran tahun: " + mobil1.tahun + "\ntransmisinya: " + mobil1.transmisi);
 
         mobil1.maju();
         mobil1.mundur();
+
+        // instance method
+        mobil1.Maju();
+
+        // manggil toString
+        System.out.println(mobil1.toString());
+
+        // manggil constructor 
+        // Mobil mobil1 = new Mobil("Pajero", "N 1000 OY", 2020);
+        // System.out.println(mobil1.merk);
+        // System.out.println(mobil1.platNomor);
+        // System.out.println(mobil1.tahun);
+
+        // Mobil mobil2 = new Mobil("Honda Jazz", "DK 5353 RA");
+        // System.out.println(mobil2.merk);
+        // System.out.println(mobil2.platNomor);
         
-        System.out.println();
+        // System.out.println();
 
     }
 }
--- MateriOop/Mobil.java
+++ MateriOop/Mobil.java
@@ -8,11 +8,31 @@ public class Mobil {
     public String merk;
 
     // harus ada setter getter kalau field nya private!!!
-    // public void setMerk(String merk) { // untuk memberikan value
+    // public void setMerk(String merk) { // UNTUK MEMBERIKAN VALUE
     //     this.merk = merk;
     // }
-    // public String getMerk() { // untuk mengambil nilai 
+    // public String getMerk() { // UNTUK MENGAMBIL NILAI
     //     return merk;
+    // }
+
+    // constructor. instansiasi sebuah objek
+    public Mobil(){
+        this.merk = "pajero";
+        this.platNomor = "N 1010 OY";
+        this.tahun = 2020;
+    }
+
+    // bisa juga langsung di assign atribut
+    // public Mobil(String merk, String platNomor, int tahun){ //harus urut yeah
+    //     this.merk = merk;
+    //     this.platNomor = platNomor;
+    //     this.tahun = tahun;
+    // }
+
+    //constructor overloading. sama tapi parameternya beda
+    // public Mobil(String merk, String platNomor){
+    //     this.merk = merk;
+    //     this.platNomor = platNomor;
     // }
 
     public void maju(){
@@ -23,4 +43,13 @@ public class Mobil {
         System.out.printf("Mobil %s dengan plat nomor %s bergerak mundur\n", merk, platNomor);
     }
 
+    // method (behaviour)
+    public void Maju(){
+        System.out.printf("%s bergerak maju", this.merk);
+    }
+
+    //method override, to String
+    public String toString(){
+        return String.format("\nNama\t\t: %s\nPlatNomor\t: %s\nTahun\t\t: %d\nTransmisi\t: %s", merk, platNomor, tahun, transmisi);
+    }
 }
